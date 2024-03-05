060fdc600b3cb0a924e956b6537e340dcaf3d292
Sir1ys <sashakoroviy@gmail.com>
2024-03-05 11:11:24 +0000

    kata is solved

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 function createChristmasTree(height) {
-  return height;
+  let array = [];
+
+  for (let i = height; i >= 0; i--) {
+    let str;
+
+    switch (i) {
+      case 0:
+        str = "|";
+        break;
+      case 1:
+        str = "x";
+        break;
+      default:
+        str = "x".repeat(i * 2 - 1);
+    }
+
+    if (i !== 0) array.unshift(str);
+    else array.push(str);
+  }
+  let maxLength = Math.max(...array.map((line) => line.length));
+  
+  let centeredText = array
+    .map((line) => {
+      let spacesToAdd = Math.floor((maxLength - line.length) / 2);
+      return " ".repeat(spacesToAdd) + line + " ".repeat(spacesToAdd);
+    })
+    .join("\n");
+
+  const result = array.join("\n");
+
+  return centeredText;
 }
 
 module.exports = createChristmasTree;
